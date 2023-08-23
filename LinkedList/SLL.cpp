dba819b79a9b9b8d581cf6ed4dfadd66a2f36070
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2023-08-23 16:58:38 +0530

    deleteLastNode added

--- LinkedList/SLL.cpp
+++ LinkedList/SLL.cpp
@@ -15,6 +15,10 @@ class SLL{
         void insertAtFirst(int);
         void insertAtLast(int);
         void insertAfter(node*,int);
+        node* searchNode(int);
+        void deleteLastNode();
+        void deleteFirstNode();
+        void deleteNode(node*);
 };
 SLL::SLL(){
     start = NULL;
@@ -31,6 +35,7 @@ void SLL::view(){
         cout<<endl;
     }
 }
+
 void SLL::insertAtLast(int data){
     node *n = new node;
     n->item = data;
@@ -39,7 +44,7 @@ void SLL::insertAtLast(int data){
         start = n;
     else{
         node *t = start;
-        while(n->next!=NULL)
+        while(t->next!=NULL)
             t=t->next;
         t->next = n;
     }
@@ -56,7 +61,54 @@ void SLL::insertAfter(node *r,int data){
     n->next = r->next;
     r->next = n;
 }
+node* SLL :: searchNode(int data){
+    node *t = start;
+    while(t!=NULL){
+        if(t->item == data)
+            return t;
+        t = t->next;
+    }
+    cout<<"Node Not Found\n";
+    return NULL;
+}
+void SLL :: deleteLastNode(){
+    if(start == NULL)
+        cout<<"List is Empty\n";
+    else{
+        node *t = start;
+        node *t1 = NULL;
+        while(t->next!=NULL){
+            t1=t;
+            t=t->next;
+        }
+        if(t1!=NULL)
+            t1->next = NULL;
+        else
+            start = NULL;
+        delete t;
+    }
+}
 int main(){
     SLL l;
+    l.insertAtLast(10);
+    l.insertAtLast(20);
+    l.insertAtLast(30);
+    l.insertAtLast(40);
+    l.insertAtLast(50);
+    l.insertAtLast(60);
+    l.insertAtFirst(999);
+    l.view();
+    node *s = l.searchNode(40);
+    l.insertAfter(s,555);
+    l.view();
+    l.deleteLastNode();
+    l.deleteLastNode();
+    l.deleteLastNode();
+    l.deleteLastNode();
+    l.deleteLastNode();
+    l.deleteLastNode();
+    l.deleteLastNode();
+    l.deleteLastNode();
+    l.view();
     return 0;
 }
