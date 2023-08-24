006413c74457a8af3a7d7dd980d59602123095b5
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2023-08-24 09:26:43 +0530

    DLL code updated, ViewRev not working properly

--- LinkedList/DLL.cpp
+++ LinkedList/DLL.cpp
@@ -8,12 +8,13 @@ class node{
         node *next;
 };
 class DLL{
-    private:
+    public:
         node *start;
     public:
         DLL();
         ~DLL();
         void view();
+        void viewRev();
         void insertAtFirst(int);
         void insertAtLast(int);
         void insertAfter(node*,int);
@@ -41,6 +42,23 @@ void DLL::view(){
         cout<<endl;
     }
 }
+void DLL:: viewRev(){
+    if(start==NULL)
+        cout<<"List is Empty\n";
+    else{
+        node *t = start;
+        while(t->next!=NULL)
+            t=t->next;
+            int x =0;
+        do{
+            cout<<t->item<<" ";
+            t=t->prev;
+            cout<<++x<<endl;
+        }while(t!=NULL);
+        // cout << ":::"<<start->next->prev << endl;
+        cout<<endl;
+    }
+}
 void DLL::insertAtLast(int data){
     node *n = new node;
     n->item = data;
@@ -53,6 +71,7 @@ void DLL::insertAtLast(int data){
         while(t->next!=NULL)
             t=t->next;
         t->next = n;
+        n->prev = t;
     }
 }
 void DLL::insertAtFirst(int data){
@@ -64,11 +83,20 @@ void DLL::insertAtFirst(int data){
 }
 void DLL::insertAfter(node *r,int data){
     node *n = new node;
-    n->item = data; 
-    n->prev = r;
-    n->next = r->next;
-    r->next->prev = n;
-    r->next = n;
+    n->item = data;
+    if(r->next==NULL){
+        n->next = NULL;
+    }
+    else if(r->prev == NULL){
+        n->next = r->next;
+        r->next->prev = n;
+    }
+    else{
+        n->next = r->next;
+        r->next->prev = n;
+    }
+        n->prev = r;
+        r->next = n;
 }
 node* DLL :: searchNode(int data){
     node *t = start;
@@ -80,10 +108,13 @@ node* DLL :: searchNode(int data){
     cout<<"Node Not Found\n";
     return NULL;
 }
-void DLL :: deleteLastNode(){
-    if(start == NULL)
+void DLL::deleteLastNode(){
+    if(start == NULL){
+        // cout<<"::";
         cout<<"List is Empty\n";
+    }
     else if(start->next == NULL){
+        // cout<<":::";
         delete start;
         start = NULL;
     }
@@ -98,8 +129,13 @@ void DLL :: deleteLastNode(){
 void DLL::deleteFirstNode(){
     if(start == NULL)
         cout<<"List is Empty\n";
+    else if(start->next == NULL){
+        delete start;
+        start = NULL;
+    }
     else{
         node *t = start;
+        t->next->prev = NULL;
         start = t->next;
         delete t;
     }
@@ -112,12 +148,14 @@ int DLL::deleteNode(node *r){
         return -1;
     }
     else if(start == r){
+        r->next->prev = NULL;
         start = r->next;
+    }else if(r->next == NULL){
+        r->prev->next = NULL;
     }
     else{
-        while(r!=t->next)
-            t=t->next;
-        t->next = r->next;
+        r->next->prev = r->prev;
+        r->prev->next = r->next;
     }
     int deletedValue = r->item;
     delete r;
@@ -131,18 +169,24 @@ int main(){
     l.insertAtLast(40);
     l.insertAtLast(50);
     l.insertAtLast(60);
+    l.insertAtLast(70);
+    // l.deleteLastNode();
     l.insertAtFirst(999);
+    // l.view();
+    // node *t = l.searchNode(20);
+    // l.insertAfter(t,6969);
     l.view();
-    node *s = l.searchNode(40);
-    l.insertAfter(s,555);
-    l.view();
-    l.deleteLastNode();
-    l.view();
-    node *s1 = l.searchNode(10);
-    int deletedData = l.deleteNode(s1);
-    cout<<"Deleted Node: "<<deletedData<<"\n";
-    l.view();
-    l.deleteFirstNode();
-    l.view();
+    l.viewRev();
+    cout<<"::"<<l.start->item<<" ";
+    // l.insertAtFirst(90);
+    // l.insertAtLast(80);
+    // l.view();
+    // l.deleteFirstNode();
+    // l.deleteFirstNode();
+    // l.deleteLastNode();
+    // l.view();
+    // int data = l.deleteNode(t);
+    // l.view();
+    // cout<<data;
     return 0;
 }
