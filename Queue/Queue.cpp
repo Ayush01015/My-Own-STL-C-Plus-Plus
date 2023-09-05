35ce1a8e1658e35ee71a0863324e9104f1f05d22
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2023-09-05 13:35:48 +0530

    Constructor and Insertion[enQueue] Added

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -2,8 +2,46 @@
 #include <iostream>
 using namespace std;
 class Queue{
-    
+    private:
+        int capacity;
+        int rear,front;
+        int *ptr;
+    public:
+        Queue();
+        Queue(int);
+        void view();
+        void enQueue(int);
+        bool isQueueFull();
+        bool isQueueEmpty();
+        int deQueue();
+        int getRear();
+        int getFront();
+        int count();
 };
+Queue::Queue(){}
+Queue::Queue(int cap){
+    capacity = cap;
+    rear = -1;
+    front = -1;
+    ptr = new int[capacity];
+}
+void Queue::enQueue(int data){
+    if(isQueueEmpty()){
+        front=0;
+        rear=0;
+        ptr[rear] = data;
+    }else if(isQueueFull()){
+        cout<<"Queue is Full\n";
+    }else if(front>0 && rear == capacity-1){
+        rear = 0;
+        ptr[rear]=data;
+    }
+    else{
+        rear++;
+        ptr[rear] = data;
+    }   
+}
+
 int main(){
     
     return 0;
